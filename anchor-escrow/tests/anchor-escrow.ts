c06b922bea2fbb0ace88f2b5ea9fb2b1ff7be8d3
xentoshi <xentoshi@protonmail.com>
2024-08-11 21:26:10 +0900

    Add anchor-escrow

---
+++ anchor-escrow/tests/anchor-escrow.ts
@@ -0,0 +1,14 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { AnchorEscrow } from "../target/types/anchor_escrow";
+
+describe("anchor-escrow", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Escrow as Program<AnchorEscrow>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+  });
+});
